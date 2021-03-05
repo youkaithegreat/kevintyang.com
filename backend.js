207d970b2f21ec6cfecb9f45e195de5a38e93c27
poplar <yangkevint@gmail.com>
2021-03-04 23:08:05 -0600

    readme and commenting

--- backend.js
+++ backend.js
@@ -1,14 +1,21 @@
-var x;
-        var counter = 0;
-        function okaybossman(){
-          
-          var element = document.getElementById("page-info");
 
+        var counter = 0;
+        //counter for the easter egg at 5 clicks, where you are redirected to my wedding photography page
+
+        function okaybossman(){
+        //button javascript function as proof of concept. 
+        //expanded functionality could be to completely switch the text in the box instead of making it disappear and/or replacing it with plain formatting
           
+        
+          var element = document.getElementById("page-info");
+          //retrieves the main text as well as the Prism container 
 
           if(element.style.display === "none" && counter < 5){
+            
             element.style.display = "block";
+            
             document.getElementById("no-more-code").innerHTML = "NO CODE!";
+            //sets button status 
             counter++;
             alert("That's what I thought. Realize your mistakes. " + counter);
 
